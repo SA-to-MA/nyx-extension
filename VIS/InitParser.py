12efbd89fa9d1824cd0ea28158704a4d93644b31
Lior <v-laftabi@microsoft.com>
2025-05-15 00:47:40 +0300

    added agents and objects mapping as well

--- VIS/InitParser.py
+++ VIS/InitParser.py
@@ -13,6 +13,8 @@ class InitParser:
         '''
         self.problem = problem
         self.init_state = {}
+        self.objects = {}
+        self.agents = {}
 
     def parse_init(self, group):
         """
@@ -95,13 +97,43 @@ class InitParser:
                 t = group.pop(0)
                 if t == ':init':
                     self.parse_init(group)
+                elif t == ':objects':
+                    self.parse_objects(group)
         else:
             raise Exception('File ' + self.problem + ' does not match problem pattern')
 
+    def parse_objects(self, input_list):
+        # Iterate through the input list to classify the items
+        i = 0
+        current_objects = []
+        while i < len(input_list):
+            if isinstance(input_list[i], list) and input_list[i][
+                0] == ':private':  # if agents private list, process and add to agents
+                private_list = input_list[i][1:]
+                agents_list = " ".join(private_list)
+                filtered = re.findall(r"([\w\s]+)-\s*([\w]+)", agents_list)
+                for item_value, item_type in filtered:
+                    for value in item_value.strip().split():
+                        if item_type not in self.agents:
+                            self.agents[item_type] = []
+                        self.agents[item_type].append(value)
+            else:
+                if input_list[i] == "-":
+                    type = input_list[i + 1]
+                    if type not in self.objects:
+                        self.objects[type] = []
+                    self.objects[type] += current_objects
+                    current_objects = []
+                    i += 1
+                else:
+                    current_objects.append(input_list[i])
+            i += 1
 
 
-# if __name__ == "__main__":
-#     # parser = InitParser("C:\\Users\\v-laftabi\Desktop\\Nyx\\nyx-extension\MA_PDDL\examples\Blocks\problem-a1.pddl")
-#     parser = InitParser("C:\\Users\\v-laftabi\Desktop\\Nyx\\nyx-extension\MA_PDDL\examples\Car\problem.pddl")
-#     parser.parse_problem()
-#     print(parser.init_state)
+if __name__ == "__main__":
+    # parser = InitParser("C:\\Users\\v-laftabi\Desktop\\Nyx\\nyx-extension\MA_PDDL\examples\Blocks\problem-a1.pddl")
+    parser = InitParser("C:\\Users\\v-laftabi\Desktop\\Nyx\\nyx-extension\MA_PDDL\examples\Car\problem.pddl")
+    parser.parse_problem()
+    print(parser.init_state)
+    print(parser.objects)
+    print(parser.agents)
